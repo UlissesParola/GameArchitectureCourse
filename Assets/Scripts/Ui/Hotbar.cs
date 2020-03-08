66ce834d116e61bc950f9a48e7113d93d2f22e35
ulissesparola <ulisses.parola@gmail.com>
2020-03-07 23:11:38 -0300

    complemento

---
+++ Assets/Scripts/Ui/Hotbar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hotbar : MonoBehaviour
+{
+    private Inventory _inventory;
+    private Slot[] _slots;
+
+
+    private void OnEnable()
+    {
+        _inventory = FindObjectOfType<Inventory>();
+        _inventory.OnItemPickedUp += HandleItemPickedUp;
+        _slots = GetComponentsInChildren<Slot>();
+    }
+
+    private void HandleItemPickedUp(Item item)
+    {
+        Slot slot = FindNextOpenSlot();
+        if (slot != null)
+        {
+            slot.SetItem(item);
+        }
+    }
+
+    private Slot FindNextOpenSlot()
+    {
+        
+        foreach (Slot slot in _slots)
+        {
+            if (slot.IsEmpty)
+            {
+                return slot;
+            }
+        }
+        
+        Debug.LogError("No empty slots available");
+        return null;
+    }
+}
