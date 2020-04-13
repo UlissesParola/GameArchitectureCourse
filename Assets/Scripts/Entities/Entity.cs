2c910fed6a97a20d6fb5d3dedde06432ec5f3bc5
ulissesparola <ulisses.parola@gmail.com>
2020-04-12 22:04:57 -0300

    Entities and NPC

---
+++ Assets/Scripts/Entities/Entity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour, ITakeHits
+{
+    [SerializeField] private int _maxHealth;
+    public int Health { get; private set; }
+
+    private void OnEnable()
+    {
+        Health = _maxHealth;
+    }
+
+    public void TakeHit(int amount)
+    {
+        Health -= amount;
+        if (Health <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            HandleNonLethalHit();
+        }
+    }
+
+    private void HandleNonLethalHit()
+    {
+        Debug.Log("Take hit");
+    }
+
+    private void Die()
+    {
+        Debug.Log("Dead");
+    }
+}
